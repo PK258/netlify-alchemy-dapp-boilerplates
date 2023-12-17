82a224e5134546710d38391131578eb163a31e56
PK258 <pradeepu258@gmail.com>
2023-12-17 10:04:12 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -67,4 +67,5 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
+<script async id="slcLiveChat" src="https://widget.sonetel.com/SonetelWidget.min.js" data-account-id="208422341"></script>
 export default MyApp;

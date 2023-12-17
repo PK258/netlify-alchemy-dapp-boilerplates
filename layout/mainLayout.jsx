b2f1431a5c751731b39258f4a3321619df549c1e
PK258 <pradeepu258@gmail.com>
2023-12-17 11:04:42 +0100

    Update mainLayout.jsx

--- layout/mainLayout.jsx
+++ layout/mainLayout.jsx
@@ -6,7 +6,5 @@ export default function MainLayout({ children }) {
             <Navbar />
             {children}
 		</div>
-		<script async id="slcLiveChat" src="https://widget.sonetel.com/SonetelWidget.min.js" data-account-id="208422341">
-		</script>
 	);
 }
